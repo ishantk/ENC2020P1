fefa112856895f609151646eac5339788f333a82
Ishant Kumar <er.ishant@gmail.com>
2020-02-27 11:54:50 +0530

    Session28 - IMDB Years Vs Ratings Plot

--- Session29E.py
+++ Session29E.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+import matplotlib.pyplot as plt
+
 url = "https://www.imdb.com/india/top-rated-indian-movies/"
 response = requests.get(url)
 
@@ -8,19 +10,33 @@ soup = BeautifulSoup(response.text, "html.parser")
 # print(soup.prettify())
 
 tags = soup.find_all("td", class_="titleColumn")
+ratingTags = soup.find_all("td", class_="ratingColumn imdbRating")
 
 movies = []
 years = []
+ratings = []
 
 for tag in tags:
     # print(tag.text)
     movieTitle = tag.text.strip()
+    movieYear = int(movieTitle[-5:-1])
     movies.append(movieTitle)
-    # print("----------")
-
-for movie in movies:
-    print(movie)
-    print("~~~~~~~~~~~")
+    years.append(movieYear)
 
 
-# Plot Histogram :)
+for tag in ratingTags:
+    rating = float(tag.text.strip())
+    ratings.append(rating)
+
+
+print(movies)
+print(len(movies))
+
+print(years)
+print(len(years))
+
+print(ratings)
+print(len(ratings))
+
+plt.barh(years, ratings)
+plt.show()
