b0493af629f2d32c1296b96249de709475f6de66
Ishant Kumar <er.ishant@gmail.com>
2020-03-17 12:25:47 +0530

    Session41 - DecisionTreeClassifier with sklearn on diabetes data-set solution

--- Session41A.py
+++ Session41A.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 # DataSet Description
 # 1. Number of times pregnant
@@ -13,3 +16,30 @@ import pandas as pd
 
 diabetesDataSet = pd.read_csv("pima-indians-diabetes.csv")
 print(diabetesDataSet)
+
+featureColumns = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age']
+
+X = diabetesDataSet[featureColumns]
+# Y = diabetesDataSet['label']
+Y = diabetesDataSet.label
+
+print("~~~~~~~~~FEATURES~~~~~~~~~~")
+print(X)
+print("~~~~~~~~~LABELS~~~~~~~~")
+print(Y)
+
+# DataSet Division -> Training and Testing
+# As we have dataset, we will not train model with all of our dataset
+# we will train the model with random dataset observations.
+# 70% shall be used to train and 30% for the testing
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=1)
+
+# Model Creation
+model = DecisionTreeClassifier()
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+print(y_pred)
+
+# Percentage Score :) How Accurate the Model IS ?
+print(">> Accuracy Score:", metrics.accuracy_score(y_test, y_pred))
