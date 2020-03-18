ef3c713be0eef181aefd02b5dfd8d8d891ddd773
Ishant Kumar <er.ishant@gmail.com>
2020-03-18 12:24:32 +0530

    Session42 - Naive Bayes Part-I

---
+++ Session42A.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+dataSet = pd.DataFrame()
+
+# Attribute: outlook
+dataSet['outlook'] = ['sunny', 'sunny', 'overcast', 'rainy', 'rainy', 'rainy',
+                      'overcast', 'sunny', 'sunny', 'rainy', 'sunny',
+                      'overcast', 'overcast', 'rainy']
+
+# Attribute: temperature
+dataSet['temperature'] = ['hot', 'hot', 'hot', 'mild', 'cool',
+                      'cool', 'cool', 'mild', 'cool', 'mild',
+                      'mild', 'mild', 'hot', 'mild']
+
+# Attribute: humidity
+dataSet['humidity'] = ['high', 'high', 'high', 'high', 'normal', 'normal',
+                      'normal', 'high', 'normal', 'normal', 'normal',
+                      'high', 'normal', 'high']
+
+# Attribute: windy
+dataSet['windy'] = ['false', 'true', 'false', 'false', 'false', 'true',
+                      'true', 'false', 'false', 'false', 'true',
+                      'true', 'false', 'true']
+# Attribute: play
+dataSet['play'] = ['no', 'no', 'yes', 'yes', 'yes', 'no',
+                      'yes', 'no', 'yes', 'yes', 'yes',
+                      'yes', 'yes', 'no']
+
+print(dataSet)
+
+# dataSet1 = pd.DataFrame()
+# dataSet1['Weather']=['Overcast','Rainy','Sunny','Grand total']
+# dataSet1['No']=[len(dataSet[dataSet['Overcast']=='no']),len(dataSet[dataSet['Rainy']=='no']),len(dataSet[dataSet['Sunny']=='no']),len(dataSet[dataSet['Overcast']=='no'])+len(dataSet[dataSet['Rainy']=='no'])+len(dataSet[dataSet['Sunny']=='no'])]
+# dataSet1['No']=[len(dataSet[dataSet['Overcast']=='yes']),len(dataSet[dataSet['Rainy']=='yes']),len(dataSet[dataSet['Sunny']=='yes']),len(dataSet[dataSet['Overcast']=='yes'])+len(dataSet[dataSet['Rainy']=='yes'])+len(dataSet[dataSet['Sunny']=='yes'])
+
+# rainyDataSet = dataSet[dataSet['outlook'] == 'rainy']
+# print(rainyDataSet)
+#
+# print("Length Of Rainy DataSet:", len(rainyDataSet))
+# print("yes_rainy", len(rainyDataSet[rainyDataSet['play'] == 'yes']))
+# print("no_rainy", len(rainyDataSet[rainyDataSet['play'] == 'no']))
+
+print("~~~~Grouping~~~~")
+count = dataSet.groupby(['outlook', 'play']).size()
+print(count, type(count))
+print("~~~~~~~~")
