9b06559a29bbe7d4a772b70fac9959b34ca3feb6
Ishant Kumar <er.ishant@gmail.com>
2020-03-19 12:26:22 +0530

    Session43 - Support Vector Machines

--- Session43B.py
+++ Session43B.py
@@ -1,4 +1,6 @@
 from sklearn import datasets, svm
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 digits = datasets.load_digits()
 
@@ -16,12 +18,21 @@ print(digits.target_names)
 print(X[11])
 print(Y[11])
 
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+
 # Create the Model
 model = svm.SVC()
 
 # Train the Model
-model.fit(X, Y)
+model.fit(x_train, y_train)
 
 # Predict the Class
-predictedClass = model.predict([X[11]])
-print(predictedClass)
+y_pred = model.predict(x_test)
+print(y_pred)
+
+# Percentage Score :) How Accurate the Model IS ?
+print(">> Accuracy Score:", metrics.accuracy_score(y_test, y_pred))
+
+# Assignment for the day : Implement SVM ML Model on Cats and Dogs Image DataSet
+# PreRequisite : Please change image into gray scale and into numpy Array. Create your own labels
+# dataset will be available on github
